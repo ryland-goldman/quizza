ryland-goldman/quizza

commit 421805c76aeaff8f7ab245d7034224fe94fd9985

Fixed relative links (#24)

html/docs/flashcards.php
@@ -27,7 +27,7 @@
       <tr>
         <td>
           <h1>
-            <a href='../'>
+            <a href='/<?php echo $className."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -36,7 +36,7 @@
     </table>
 
     <p>
-      <a href='../' id='back-link'>
+      <a href='<?php echo $className."/".$setID; ?>' id='back-link'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>

html/docs/fr.php
@@ -30,7 +30,7 @@
       <tr>
         <td>
           <h1>
-            <a href='../'>
+            <a href='<?php echo $className."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -39,7 +39,7 @@
     </table>
 
     <p id="back-link">
-      <a href='../'>
+      <a href='<?php echo $className."/".$setID; ?>'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>

html/docs/match.php
@@ -20,7 +20,7 @@
       <tr>
         <td>
           <h1>
-            <a href='../'>
+            <a href='<?php echo $className."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -29,7 +29,7 @@
     </table>
 
     <p id='back-link'>
-      <a href='../'>
+      <a href='<?php echo $className."/".$setID; ?>'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>

html/docs/mc.php
@@ -45,7 +45,7 @@
       <tr>
         <td>
           <h1>
-            <a href='../'>
+            <a href='<?php echo $className."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -54,7 +54,7 @@
     </table>
 
     <p id='back-link'>
-      <a href='../'>
+      <a href='<?php echo $className."/".$setID; ?>'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>

html/docs/saveChanges.php
@@ -21,7 +21,7 @@ if($_GET["DELETE"]=="TRUE"){ // Deleting a set
   $admin->query("DELETE FROM ".$classID."Sets WHERE ID=\"$setID\"");
   $thisClass->query("DROP TABLE IF EXISTS Archive".$type.$setID);
   $thisClass->query("ALTER TABLE ".$type.$setID." RENAME Archive".$type.$setID);
-  die("<script>location.href='../../';</script>");
+  die("<script>location.href='/".$classID."';</script>");
 } else {
   if($type == "Set"){
     $thisClass->query("DELETE FROM Set".$setID);

commit a776e82cf976e1cb916da52268da83318d65d20e

Fixed issue caused by 421805c and 5824498

html/docs/flashcards.php
@@ -27,7 +27,7 @@
       <tr>
         <td>
           <h1>
-            <a href='/<?php echo $className."/".$setID; ?>'>
+            <a href='/<?php echo $classID."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -36,7 +36,7 @@
     </table>
 
     <p>
-      <a href='/<?php echo $className."/".$setID; ?>' id='back-link'>
+      <a href='/<?php echo $classID."/".$setID; ?>' id='back-link'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>

html/docs/fr.php
@@ -30,7 +30,7 @@
       <tr>
         <td>
           <h1>
-            <a href='/<?php echo $className."/".$setID; ?>'>
+            <a href='/<?php echo $classID."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -39,7 +39,7 @@
     </table>
 
     <p id="back-link">
-      <a href='/<?php echo $className."/".$setID; ?>'>
+      <a href='/<?php echo $classID."/".$setID; ?>'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>

html/docs/match.php
@@ -9,7 +9,7 @@
   <script async>
     var answers = [<?php $words = $thisClass->query("SELECT * FROM ".$type.$setID); if($words->num_rows > 0){ while($row = $words->fetch_assoc()){ echo '[`'.str_replace("\\","\\\\",$row[$type=="Set"?"Term":"Question"]).'`,`'.str_replace("\\","\\\\",$row[$type=="Set"?"Definition":"C1"]).'`],'; } } ?>[]];
     answers.pop();
-    if(answers.length < 16){ location.href = "<?php echo $className."/".$setID; ?>"; }
+    if(answers.length < 16){ location.href = "<?php echo $classID."/".$setID; ?>"; }
   </script>
 </head>
 
@@ -21,7 +21,7 @@
       <tr>
         <td>
           <h1>
-            <a href='/<?php echo $className."/".$setID; ?>'>
+            <a href='/<?php echo $classID."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -30,7 +30,7 @@
     </table>
 
     <p id='back-link'>
-      <a href='/<?php echo $className."/".$setID; ?>'>
+      <a href='/<?php echo $classID."/".$setID; ?>'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>

html/docs/mc.php
@@ -45,7 +45,7 @@
       <tr>
         <td>
           <h1>
-            <a href='/<?php echo $className."/".$setID; ?>'>
+            <a href='/<?php echo $classID."/".$setID; ?>'>
               <?php echo $class_icon."&nbsp;&nbsp;".$setName; ?>
             </a>
           </h1>
@@ -54,7 +54,7 @@
     </table>
 
     <p id='back-link'>
-      <a href='/<?php echo $className."/".$setID; ?>'>
+      <a href='/<?php echo $classID."/".$setID; ?>'>
         <i class="fa-solid fa-arrow-left"></i> Back
       </a>
     </p>