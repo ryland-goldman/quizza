dc78fc907c376d339e5a37dd584051f93b901dd4
Ryland Goldman <ryland@rylandgoldman.com>
2023-07-20 15:57:06 -0700

    Fixed the wrong problem in 148c8a1

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -2,6 +2,6 @@
 <div id='footer'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:#eee;'<?php } ?>>
     <p>
         Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
-        For feedback or suggestions, use our<a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a>&nbsp; page.
+        For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a> page.
     </p>
 </div>
