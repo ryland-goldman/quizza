6befb09f359a7678f68336e419d2e05a44034c05
Ryland Goldman <ryland@rylandgoldman.com>
2023-07-20 15:54:52 -0700

    Added GitHub link to footer (#19)

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -1,6 +1,7 @@
 <!-- Footer -->
 <div id='footer'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:#eee;'<?php } ?>>
-    <p>Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>Click 
-        <a target="_blank" href='https://forms.gle/gwCVXPSdpjpnWgbZ7'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>here</a>
-     to send feedback or suggestions.</p>
+    <p>
+        Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
+        For feedback or suggestions, use our<a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a> page.
+    </p>
 </div>
