5187038550d48c05f9a8978777606528e216cf76
Ryland Goldman <ryland@rylandgoldman.com>
2023-07-23 16:43:43 -1000

    Missing 404.php in 9b43a88

---
+++ html/404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title>Quizza | 404</title>
+	<?php require("/var/www/html/docs/lib/imports.php"); ?>
+
+</head>
+
+<body id='body-404'>
+
+  <h1 id='title-404'>404</h1>
+	<a href='https://www.quizza.org/'>
+		<button id='button-404'>Home</button>
+	</a>
+
+</body>
+
+</html>
