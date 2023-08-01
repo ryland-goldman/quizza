b5b15bdd832a538ef415b81fa96c80a4b1ba0d41
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-01 16:35:13 -0700

    Homepage (#7)

---
+++ html/main.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quizza</title>
+</head>
+<body>
+	<img src='https://www.quizza.org/static/images/main-1.png' style='width:100vw;'>
+	<img src='https://www.quizza.org/static/images/main-2.png' style='width:100vw;'>
+	</body>
+</head>
