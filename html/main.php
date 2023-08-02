49fac3995d678d09f92c52db0604d5bb867312b9
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-01 19:05:26 -0700

    Updated homepage

--- html/main.php
+++ html/main.php
@@ -1,3 +1,4 @@
+<?php require("/var/www/html/docs/lib/header.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,28 +25,48 @@
   <div><table><tr><td style='padding:13px 20px 5px;'><h1>&nbsp;</h1></td></tr></table></div>
 
 	<div id='homepage-bgimage-1'>&nbsp;</div>
-	<div id='sep'>
-		<table>
-			<tr>
-				<td><h1>Choose your school</h1></td>
-				<td>
-			    	<div class="dropdown">
-			        	<div class="select">
-			        		<span>Select School</span>
-			        		<i class="fa-solid fa-chevron-down"></i>
-			        	</div>
-			        	<input type="hidden" name="school">
-				        <ul class="dropdown-menu">
-				        	<li id="lghs">Los Gatos High School</li>
-				        	<li id="rjf">R.J. Fisher Middle School</li>
-				        	<li id="shs">Saratoga High School</li>
-				        	<li id="ucla">University of California, Los Angeles</li>
-				        </ul>
-			      	</div>
-				</td>
-			</tr>
-		</table>
-	</div>
+	<?php if(!isMobileDevice()){ ?>
+		<div id='sep'>
+			<table>
+				<tr>
+					<td><h1>Choose your school</h1></td>
+					<td>
+				    	<div class="dropdown">
+				        	<div class="select">
+				        		<span>Select School</span>
+				        		<i class="fa-solid fa-chevron-down"></i>
+				        	</div>
+				        	<input type="hidden" name="school">
+					        <ul class="dropdown-menu">
+					        	<li id="lghs">Los Gatos High School</li>
+					        	<li id="rjf">R.J. Fisher Middle School</li>
+					        	<li id="shs">Saratoga High School</li>
+					        	<li id="ucla">University of California, Los Angeles</li>
+					        </ul>
+				      	</div>
+					</td>
+				</tr>
+			</table>
+		</div>
+	<? } else { ?>
+		<div id='sep'>
+			<h1>Choose your school</h1>
+			<br>
+			<div class="dropdown">
+		     	<div class="select">
+			        <span>Select School</span>
+			       	<i class="fa-solid fa-chevron-down"></i>
+			    </div>
+			   	<input type="hidden" name="school">
+			    <ul class="dropdown-menu">
+		        	<li id="lghs">Los Gatos High School</li>
+		        	<li id="rjf">R.J. Fisher Middle School</li>
+		        	<li id="shs">Saratoga High School</li>
+		        	<li id="ucla">University of California, Los Angeles</li>
+		        </ul>
+	      	</div>
+		</div>
+	<?php } ?>
 	<img src='https://www.quizza.org/static/images/main-2.webp' style='width:100vw;'>
 </body>
 </head>
