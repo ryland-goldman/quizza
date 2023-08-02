f8c6f2de7d490a6f38e9622b0a1ec02285cd581f
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-01 19:06:17 -0700

    Updated homepage

--- html/main.php
+++ html/main.php
@@ -1,4 +1,4 @@
-<?php require("/var/www/html/docs/lib/header.php"); ?>
+<?php function isMobileDevice() { return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]); } ?>
 <!DOCTYPE html>
 <html>
 <head>
