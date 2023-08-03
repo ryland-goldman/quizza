14ed0c1ab14d2ba42f43776b1a265466f4b7dd30
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-02 19:06:14 -0700

    Fixed issues with 9f9e277

--- html/docs/lib/accessLogAppend.php
+++ html/docs/lib/accessLogAppend.php
@@ -2,7 +2,7 @@
 $sql_db_password =  trim(file_get_contents("/var/www/sql.privkey"));
 $admin1 = new mysqli("localhost", "quizza", $sql_db_password, "AccessLog");
 $timestamp = time();
-$school1 = "";
+$school1 = "homepage";
 $class1 = "";
 $set1 = "";
 $section1 = "";
@@ -14,6 +14,6 @@ if(isset($setID)){ $class1 = $admin1->real_escape_string(filter_var($setID,FILTE
 if(isset($setID)){ $section1 = basename($_SERVER['PHP_SELF']); }
 if(isset($email)){ $user1 = $admin1->real_escape_string(filter_var($email,FILTER_SANITIZE_STRING)); }
 if(isset($_GET["ref"])){ $ref1 = $_GET["ref"]; }
-$admin1->query("INSERT INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\"");
+$admin1->query("INSERT INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\")");
 addToLog();
 ?>
