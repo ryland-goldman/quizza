5911f896ae004c00d782f96735da25c5aaea3d59
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-02 19:04:17 -0700

    Fixed issues with 9f9e277

--- html/docs/lib/accessLogAppend.php
+++ html/docs/lib/accessLogAppend.php
@@ -1,22 +1,19 @@
 <?php
 $sql_db_password =  trim(file_get_contents("/var/www/sql.privkey"));
-// Function keeps variables local
-function addToLog(){
-	$admin1 = new mysqli("localhost", "quizza", $sql_db_password, "AccessLog");
-	$timestamp = time();
-	$school1 = "";
-	$class1 = "";
-	$set1 = "";
-	$section1 = "";
-	$user1 = "";
-	$ref1 = "";
-	if(isset($school)) { $school1 = $admin1->real_escape_string(filter_var($school,FILTER_SANITIZE_STRING)); }
-	if(isset($classID)){ $class1 = $admin1->real_escape_string(filter_var($classID,FILTER_SANITIZE_STRING)); }
-	if(isset($setID)){ $class1 = $admin1->real_escape_string(filter_var($setID,FILTER_SANITIZE_STRING)); }
-	if(isset($setID)){ $section1 = basename($_SERVER['PHP_SELF']); }
-	if(isset($email)){ $user1 = $admin1->real_escape_string(filter_var($email,FILTER_SANITIZE_STRING)); }
-	if(isset($_GET["ref"])){ $ref1 = $_GET["ref"]; }
-	$admin->query("INSERT INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\"");
-}
+$admin1 = new mysqli("localhost", "quizza", $sql_db_password, "AccessLog");
+$timestamp = time();
+$school1 = "";
+$class1 = "";
+$set1 = "";
+$section1 = "";
+$user1 = "";
+$ref1 = "";
+if(isset($school)) { $school1 = $admin1->real_escape_string(filter_var($school,FILTER_SANITIZE_STRING)); }
+if(isset($classID)){ $class1 = $admin1->real_escape_string(filter_var($classID,FILTER_SANITIZE_STRING)); }
+if(isset($setID)){ $class1 = $admin1->real_escape_string(filter_var($setID,FILTER_SANITIZE_STRING)); }
+if(isset($setID)){ $section1 = basename($_SERVER['PHP_SELF']); }
+if(isset($email)){ $user1 = $admin1->real_escape_string(filter_var($email,FILTER_SANITIZE_STRING)); }
+if(isset($_GET["ref"])){ $ref1 = $_GET["ref"]; }
+$admin->query("INSERT INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\"");
 addToLog();
 ?>
