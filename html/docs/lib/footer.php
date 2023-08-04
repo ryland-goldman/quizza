75d7894fac1b055ef47a1912031765971919ffd1
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-04 16:26:23 -0700

    Buy me a coffee

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -4,7 +4,7 @@
         Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
         For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a> page.
     </p>
-    <?php if(!isset($bmac)) { ?>
+    <?php if(!isset($bmac) && !isMobileDevice()) { ?>
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="quizza" data-description="Support me on Buy me a coffee!" data-message="" data-color="#40DCA5" data-position="Center" data-x_margin="18" data-y_margin="18"></script>
     <?php } ?>
 </div>
