7a35621a22ea624ad6a7c96849f9677136fb3576
Ryland Goldman <ryland@quizza.org>
2023-08-05 23:02:41 +0100

    Email

--- html/docs/lib/share_private_set.php
+++ html/docs/lib/share_private_set.php
@@ -8,5 +8,9 @@ if(!isset($_GET["email"]) || !isset($_GET["edit_permission"])){ header("HTTP 400
 $allowed_obj[$_GET["email"]] = ($_GET["edit_permission"]=="true") ? 2 : 1;
 $encoded = base64_encode(json_encode($allowed_obj));
 $admin->query("UPDATE privateSets SET Shared=\"".$encoded."\" WHERE ID=\"".$setID."\"");
+$to_email = $_GET["email"];
+$from_name = $name;
+$setname = $setName;
+require("/var/www/html/docs/lib/email-endpoint/index.php");
 echo "success";
 ?>
