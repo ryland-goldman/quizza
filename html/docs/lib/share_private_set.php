0327d2db5d0d4df3b7df527d60358ce7de67820c
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-05 13:33:27 -0700

    Added sharing sets

---
+++ html/docs/lib/share_private_set.php
@@ -0,0 +1,12 @@
+<?php
+$classID = "private";
+$req_permission = 3;
+require("/var/www/html/docs/lib/header.php");
+if(!$private_set){ header("HTTP 400 Bad Request"); die(); }
+$allowed_obj = json_decode($allowed,true);
+if(!isset($_GET["email"]) || !isset($_GET["edit_permission"])){ header("HTTP 400 Bad Request"); die(); }
+$allowed_obj[$_GET["email"]] = ($_GET["edit_permission"]=="true");
+$encoded = base64_encode(json_encode($allowed_obj));
+$admin->query("UPDATE TABLE privateSets SET Shared=\""+$encoded+"\" WHERE ID=\""+$setID+"\"");
+echo "success";
+?>
