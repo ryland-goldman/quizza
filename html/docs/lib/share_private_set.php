8854236536cde3d4a425a6c1d7b6872cb8999452
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-05 13:34:23 -0700

    Issue with 0327d2d

--- html/docs/lib/share_private_set.php
+++ html/docs/lib/share_private_set.php
@@ -7,6 +7,6 @@ $allowed_obj = json_decode($allowed,true);
 if(!isset($_GET["email"]) || !isset($_GET["edit_permission"])){ header("HTTP 400 Bad Request"); die(); }
 $allowed_obj[$_GET["email"]] = ($_GET["edit_permission"]=="true");
 $encoded = base64_encode(json_encode($allowed_obj));
-$admin->query("UPDATE TABLE privateSets SET Shared=\""+$encoded+"\" WHERE ID=\""+$setID+"\"");
+$admin->query("UPDATE TABLE privateSets SET Shared=\"".$encoded."\" WHERE ID=\"".$setID."\"");
 echo "success";
 ?>
