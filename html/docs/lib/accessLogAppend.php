327cb15105d99963a6dc52d3325eca46bd690fea
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-06 09:30:17 -0700

    Prevent access log deadlock

--- html/docs/lib/accessLogAppend.php
+++ html/docs/lib/accessLogAppend.php
@@ -15,5 +15,19 @@ if(isset($setID)){ $set1 = $admin1->real_escape_string(filter_var($setID,FILTER_
 if(isset($setID)){ $section1 = basename($_SERVER['PHP_SELF']); }
 if(isset($email)){ $user1 = $admin1->real_escape_string(filter_var($email,FILTER_SANITIZE_STRING)); }
 if(isset($_GET["ref"])){ $ref1 = $_GET["ref"]; }
-$admin1->query("INSERT IGNORE INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\",\"$ipaddr\")");
+
+// Attempt to add to log three times (to avoid SQL deadlock)
+try {
+	$admin1->query("INSERT IGNORE INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\",\"$ipaddr\")");
+} catch (Exception $e){
+	try {
+		$admin1->query("INSERT IGNORE INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\",\"$ipaddr\")");
+	} catch (Exception $e){
+		try {
+			$admin1->query("INSERT IGNORE INTO AccessLog VALUES (\"$timestamp\",\"$school1\",\"$class1\",\"$set1\",\"$section1\",\"$user1\",\"$ref1\",\"$ipaddr\")");
+		} catch (Exception $e){
+			// Failed to add to log
+		}
+	}
+}
 ?>
