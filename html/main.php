0b87e5da08103f083176132e2277fb349b66992c
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-06 13:56:10 -0700

    Change dropdown

--- html/main.php
+++ html/main.php
@@ -37,7 +37,6 @@
   <div><table><tr><td style='padding:13px 20px 5px;'><h1>&nbsp;</h1></td></tr></table></div>
 
 	<div id='homepage-bgimage-1'>&nbsp;</div>
-	<?php if(!isMobileDevice()){ ?>
 		<div id='sep'>
 			<table>
 				<tr>
@@ -62,27 +61,6 @@
 				</tr>
 			</table>
 		</div>
-	<?php } else { ?>
-		<div id='sep'>
-			<h1>Choose your school</h1>
-			<hr>
-			<div class='select'>
-				<select>
-					<option value="www" selected>Select a School</option>
-					<option value="private">Private Sets (No School Needed)</option>
-					<?php $schooldb = new mysqli("localhost","quizza", trim(file_get_contents("/var/www/sql.privkey")), "Schools");
-					$schools = $schooldb->query("SELECT * FROM main ORDER BY longname ASC;");
-					while($curr_school = $schools->fetch_assoc()){ 
-					  if($curr_school["shortname"] !== $school_shortname){ ?>
-					    <option value="<?php echo $curr_school["id"]; ?>"><?php echo $curr_school["longname"]; ?></option>
-					<?php } } ?>
-				</select>
-				<div class="select-after">
-					<i class="fa-solid fa-caret-down"></i>&nbsp;
-				</div>
-			</div>
-		</div>
-	<?php } ?>
 	<img src='https://www.quizza.org/static/images/<?php if(isMobileDevice()){ ?>mobile<?php } else { ?>main<?php } ?>-2.png' style='width:100vw;'>
 	<?php $bmac = false; require("/var/www/html/docs/lib/footer.php"); ?>
 
