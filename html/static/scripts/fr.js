fc36b43b239dd876268704aa6f971a8e337c76db
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-05 17:13:17 -0700

    Fix #51

--- html/static/scripts/fr.js
+++ html/static/scripts/fr.js
@@ -35,7 +35,7 @@ function submit(){
         score++;
         $("#main-td").html("<h1 style='color:green'>Correct</h1><p>"+words[current_flashcard]+": "+defs[current_flashcard]+"</p>");
     } else {
-        $("#main-td").html("<h1 style='color:red'>Incorrect</h1><p>"+words[current_flashcard]+": "+defs[current_flashcard]+"<br>You said: <span color='#777'>"+answer+"</span> (<a href='javascript:override();'>override</a>)</p>");
+        $("#main-td").html("<h1 style='color:red'>Incorrect</h1><p>"+words[current_flashcard]+": "+defs[current_flashcard]+"<br>You said: <span color='#777'>"+answer+"</span><br><br><a href='javascript:override();' style='text-decoration:underline;'>mark correct</a></p>");
         missed.push(question);
     }
     $("#sbtn").html(`Next&nbsp;&nbsp;<i class="fa-solid fa-arrow-right-to-bracket"></i>`);
