1366942224882151f2be5fa62f678d7d4cc414a4
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-06 14:30:56 -0700

    Fix 132a172

--- html/docs/lib/topBar.php
+++ html/docs/lib/topBar.php
@@ -9,40 +9,47 @@
         </td>
         <td>&nbsp;</td>
         <td id="top-bar-upper-icon">
-          <div id="topbar-menuToggle">
-            <input type="checkbox" />
-            <span></span>
-            <span></span>
-            <span></span>
-            <div id="topbar-menu">
-              <p>
-                <?php if($loggedIn){ ?>
-                  <table style='float:right;'>
-                    <tr>
-                      <td>
-                        <a href='https://www.quizza.org/private' id='top-bar-name'>Welcome, <?php echo $name; ?></a><br>
-                        <a href='javascript:signout()' id='top-bar-signout'>Sign Out</a>
-                      </td>
-                      <td>
-                        <img src='<?php echo $pic; ?>' id='top-bar-icon-image'>
-                      </td>
-                    </tr>
-                  </table>
-                <?php } else { ?><div id="buttonDiv"></div><?php } ?>
-              </p>
-              <hr>
-              <p>Change School</p>
-              <select>
-                <?php if(!$private_set){ ?><option value="www" selected><?php echo $school_shortname; ?></option><?php } ?>
-                <option value="private">Private Sets</option>
-                <?php $schools = $schooldb->query("SELECT * FROM main");
-                while($curr_school = $schools->fetch_assoc()){ 
-                  if($curr_school["shortname"] !== $school_shortname){ ?>
-                    <option value="<?php echo $curr_school["id"]; ?>"><?php echo $curr_school["shortname"]; ?></option>
-                <?php } } ?>
-              </select>
-            </div>
-          </div>
+          <table style='float:right;'>
+            <tr>
+              <td>&nbsp;</td>
+              <td>
+                <div id="topbar-menuToggle">
+                  <input type="checkbox" />
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <div id="topbar-menu">
+                    <p>
+                      <?php if($loggedIn){ ?>
+                        <table style='float:right;'>
+                          <tr>
+                            <td>
+                              <a href='https://www.quizza.org/private' id='top-bar-name'>Welcome, <?php echo $name; ?></a><br>
+                              <a href='javascript:signout()' id='top-bar-signout'>Sign Out</a>
+                            </td>
+                            <td>
+                              <img src='<?php echo $pic; ?>' id='top-bar-icon-image'>
+                            </td>
+                          </tr>
+                        </table>
+                      <?php } else { ?><div id="buttonDiv"></div><?php } ?>
+                    </p>
+                    <hr>
+                    <p>Change School</p>
+                    <select>
+                      <?php if(!$private_set){ ?><option value="www" selected><?php echo $school_shortname; ?></option><?php } ?>
+                      <option value="private">Private Sets</option>
+                      <?php $schools = $schooldb->query("SELECT * FROM main");
+                      while($curr_school = $schools->fetch_assoc()){ 
+                        if($curr_school["shortname"] !== $school_shortname){ ?>
+                          <option value="<?php echo $curr_school["id"]; ?>"><?php echo $curr_school["shortname"]; ?></option>
+                      <?php } } ?>
+                    </select>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </table>
         </td>
       </tr>
     </table>
