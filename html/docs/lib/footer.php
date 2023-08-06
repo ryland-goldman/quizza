52e692c78c6fa303141ba7ab462df9297a174a2e
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-06 11:56:48 -0700

    Signin button footer

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<div id='footer'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:#eee;  margin-left: auto; margin-right: auto;'<?php } ?>>
+<div id='footer'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:#eee;'<?php } ?>>
     <p>
         Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
         For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a> page.<br>
