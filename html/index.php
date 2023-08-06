d544e70b37fc4a4e251b37ae7ad2d4a704b10726
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-06 14:06:26 -0700

    Change dropdown

--- html/index.php
+++ html/index.php
@@ -16,6 +16,9 @@
     <div class="custom-select">
     <h1 id="fontpage-header-large">
       Access Free Study Materials for
+      <?php if(isMobileDevice()) { ?>
+        <a style="text-decoration:underline;color:white;" href='https://www.quizza.org/#sep'><?php echo $school_shortname; ?></a>
+      <?php } else { ?>
       <select id="school-selector">
         <option value="www" selected><?php echo $school_shortname; ?></option>
         <option value="private">Private Sets</option>
