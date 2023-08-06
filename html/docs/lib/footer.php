5b1b97c9386de68998a37d84e3d167822b1b2076
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-06 09:41:56 -0700

    More verbose search

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -4,6 +4,7 @@
         Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
         For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a> page.<br>
         <a href='https://www.quizza.org/static/files/TOS.html' <?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>Privacy and Terms</a>
+        <?php if(!$loggedIn){ ?> • <a href="javascript:google_signin_footer()">Sign In</a><?php } else { ?> • Signed In As <?php echo $name; } ?>
     </p>
     <?php if(!isset($bmac) && !isMobileDevice()) { ?>
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="quizza" data-description="Support me on Buy me a coffee!" data-message="" data-color="#40DCA5" data-position="Center" data-x_margin="18" data-y_margin="18"></script>
--- html/docs/lib/searchClasses.php
+++ html/docs/lib/searchClasses.php
@@ -3,17 +3,28 @@ $ignoreLog = true;
 require("/var/www/html/docs/lib/header.php");
 $query = $admin->real_escape_string(filter_var(urldecode($_GET["query"]),FILTER_SANITIZE_STRING));
 $result = $admin->query("SELECT * FROM Classes WHERE ShortName LIKE \"".$query."%\" OR ShortName=\"".$query."\" ORDER BY ShortName ASC LIMIT 10;");
+$result2 = $admin->query("SELECT * FROM Classes WHERE LongName LIKE \"".$query."%\" OR LongName=\"".$query."\" ORDER BY LongName ASC LIMIT 10;");
+$result3 = $admin->query("SELECT * FROM Classes WHERE ShortName LIKE \"%".$query."%\" ORDER BY ShortName ASC LIMIT 10;");
+$result4 = $admin->query("SELECT * FROM Classes WHERE LongName LIKE \"%".$query."%\" ORDER BY LongName ASC LIMIT 10;");
 
+$stack = array();
 $first = true;
-if($result->num_rows > 0){
+if($result->num_rows > 0 && $result2->num_rows > 0 && $result3->num_rows > 0 && $result4->num_rows > 0){
 	echo "[";
- 	while ($class = $result->fetch_assoc()) {
-		if(!$first){ echo ","; }
-	 	if($first){ $first = false;}
-	 	$shortname = $class["ShortName"];
-	 	$id = $class["ID"];
-	 	echo "[\"$shortname\",\"$id\"]";
- 	}
+	$results = [$result, $result2, $result3, $result4];
+	foreach($results as $result){
+		if($result->num_rows > 0){
+		 	while ($class = $result->fetch_assoc()) {
+			 	$shortname = $class["ShortName"];
+			 	$id = $class["ID"];
+			 	if(in_array($id,$stack)){ continue; }
+				if(!$first){ echo ","; }
+			 	if($first){ $first = false;}
+			 	echo "[\"$shortname\",\"$id\"]";
+			 	array_push($stack, $id);
+		 	}
+		}
+	}
  	echo "]";
 } else {
 	echo "";
