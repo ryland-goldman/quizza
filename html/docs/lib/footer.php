3f227c7820b3ef5cb30ebce5659ad2298f3003c4
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-06 15:01:59 -0700

    Fix 132a172

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -4,20 +4,6 @@
         Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
         For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a> page.<br>
         <a href='https://www.quizza.org/static/files/TOS.html' <?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>Privacy and Terms</a>
-         <br>
-         <table>
-            <tr>
-                <?php if(!$loggedIn){ ?>
-                    <td><p>Sign In With Google</p></td>
-                    <td>
-                        <div class="g_id_signin" data-type="icon" data-shape="square" data-theme="outline" data-text="signin" data-size="medium"></div>
-                        <div id="g_id_onload" data-client_id="117895756240-ujiuojlsbtruthgqnghnu215d2hn7flp.apps.googleusercontent.com" data-context="signin" data-ux_mode="popup" data-callback="handleCredentialResponse" data-auto_prompt="false"></div>
-                    </td>
-                <?php } else { ?>
-                    <td><p>Signed in as <strong><?php echo $name; ?></strong></p></td>
-                <?php } ?>
-            </tr>
-        </table>
     </p>
     <?php if(!isset($bmac) && !isMobileDevice()) { ?>
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="quizza" data-description="Support me on Buy me a coffee!" data-message="" data-color="#40DCA5" data-position="Center" data-x_margin="18" data-y_margin="18"></script>
