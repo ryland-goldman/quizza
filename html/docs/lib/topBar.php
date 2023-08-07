d1ad43be04b757541786d1bb0fb03fe957f3dae1
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-07 13:01:00 -0700

    Revert deb9edc

--- html/docs/lib/topBar.php
+++ html/docs/lib/topBar.php
@@ -9,7 +9,6 @@
           </a>
         </td>
         <td id="top-bar-upper-icon" class='td-outer'>
-          <?php if(isMobileDevice()){ ?>
           <table style='width:100%'>
             <tr>
               <td style='float:right;'>
@@ -73,55 +72,6 @@
                     </div>
                   </div>
                 </div>
-                <?php } else { ?>
-                  <div class='select'>
-                    <select>
-                      <?php if($_SERVER['HTTP_HOST']=="www.quizza.org"){ ?><option selected>Select a School</option><?php } ?>
-                      <?php try {
-                        $sql_db_password =  trim(file_get_contents("/var/www/sql.privkey"));
-                        $schooldb = new mysqli("localhost","quizza", $sql_db_password, "Schools");
-                      } catch (Exception $e) {}
-                      $schools = $schooldb->query("SELECT * FROM main ORDER BY longname ASC;");
-                      while($curr_school = $schools->fetch_assoc()){ ?>
-                        <option value="<?php echo $curr_school["id"]; ?>"<?php if($curr_school["shortname"] == $school_shortname){ ?> selected<?php } ?>><?php echo $curr_school["longname"]; ?></option>
-                      <?php } ?>
-                    </select>
-                    <div class="select-after">
-                      <i class="fa-solid fa-caret-down"></i>&nbsp;
-                    </div>
-                  </div>
-                  <?php if($loggedIn){ ?>
-                        <table id='tr-border-bottom-table'>
-                          <tr>
-                            <td>
-                              <img src='<?php echo $pic; ?>' id='top-bar-icon-image'>
-                            </td>
-                            <td>
-                              <a href='https://www.quizza.org/private' id='top-bar-name'>Welcome, <?php echo $name; ?></a><br>
-                              <a href='javascript:signout()' id='top-bar-signout'>Sign Out</a>
-                            </td>
-                          </tr>
-                          <tr id='tr-border-bottom'>
-                            <td>&nbsp;</td>
-                            <td>&nbsp;</td>
-                          </tr>
-                        </table>
-                    <?php } else { ?>
-                      <table id='tr-border-bottom-table'>
-                          <tr>
-                            <td>
-                              <div id="buttonDiv"></div>
-                            </td>
-                            <td>
-                              &nbsp;
-                            </td>
-                          </tr>
-                          <tr id='tr-border-bottom'>
-                            <td>&nbsp;</td>
-                          </tr>
-                        </table>
-                    <?php } ?>
-                <?php } ?>
               </td>
             </tr>
           </table>
