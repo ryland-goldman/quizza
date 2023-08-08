fb957be5b01db87e8547c734f1103d77ff1adaba
Ryland Goldman <ryland@quizza.org>
2023-08-08 03:36:00 +0100

    cleanup

--- html/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$classID = "private";
-$req_permission = 3;
-require("/var/www/html/docs/lib/header.php");
-if(!$private_set){ header("HTTP 400 Bad Request"); die(); }
-echo $allowed;
-$allowed_obj = json_decode($allowed,true);
-echo json_encode($allowed_obj);
-?>
