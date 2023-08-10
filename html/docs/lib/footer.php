90ba8948866daacff4ff8ec7712fda88a072e950
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-10 12:38:22 -0700

    Fix footer

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -1,9 +1,9 @@
 <!-- Footer -->
-<div id='footer'<?php if($_SERVER['PHP_SELF']=="/index.php" || $_SERVER['PHP_SELF']=="/main.php") { ?> style='color:#eee;'<?php } ?>>
+<div id='footer'<?php if($_SERVER['PHP_SELF']=="/index.php" && !isset($mainphp)) { ?> style='color:#eee;'<?php } ?>>
     <p>
         Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
-        For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>GitHub</a> page.<br>
-        <a href='https://www.quizza.org/static/files/TOS.html' <?php if($_SERVER['PHP_SELF']=="/index.php") { ?> style='color:white;'<?php } ?>>Privacy and Terms</a>
+        For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php" && !isset($mainphp)) { ?> style='color:white;'<?php } ?>>GitHub</a> page.<br>
+        <a href='https://www.quizza.org/static/files/TOS.html' <?php if($_SERVER['PHP_SELF']=="/index.php" && !isset($mainphp)) { ?> style='color:white;'<?php } ?>>Privacy and Terms</a>
     </p>
 </div>
 <?php if(!isset($bmac) && !isMobileDevice()) { ?>
