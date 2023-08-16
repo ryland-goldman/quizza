fe7dc780556d2fb40fea91b658c2d01c0df806f3
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-16 08:14:52 -0700

    Fix #87

--- html/docs/lib/footer.php
+++ html/docs/lib/footer.php
@@ -3,7 +3,7 @@
     <p>
         Copyright &copy; 2022-23 Ryland Goldman and Collin Wentzien<br>
         For feedback or suggestions, use our <a target="_blank" href='https://github.com/ryland-goldman/quizza/issues'<?php if($_SERVER['PHP_SELF']=="/index.php" && !isset($mainphp)) { ?> style='color:white;'<?php } ?>>GitHub</a> page.<br>
-        <a href='https://www.quizza.org/static/files/TOS.html' <?php if($_SERVER['PHP_SELF']=="/index.php" && !isset($mainphp)) { ?> style='color:white;'<?php } ?>>Privacy and Terms</a>
+        <a href='https://www.quizza.org/static/files/TOS.html' <?php if($_SERVER['PHP_SELF']=="/index.php" && !isset($mainphp)) { ?> style='color:white;'<?php } ?>>Privacy and Terms</a> • <a href='https://forms.gle/YfAH3qHdQGswbfN39' target="_blank" <?php if($_SERVER['PHP_SELF']=="/index.php" && !isset($mainphp)) { ?> style='color:white;'<?php } ?>>Report Content</a>
     </p>
 </div>
 <?php if(!isset($bmac) && !isMobileDevice()) { ?>
