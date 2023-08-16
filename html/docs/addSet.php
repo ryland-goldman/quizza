c7ed38cd76cffc82b2a507c19658c09ab2fe29e6
Ryland Goldman <ryland@rylandgoldman.com>
2023-08-16 14:19:08 -0700

    Fix #17

--- html/docs/addSet.php
+++ html/docs/addSet.php
@@ -8,16 +8,17 @@ $lastModified = date("d M Y (h:i A T)")." by ".$name;
 $admin->query("LOCK TABLES ".$classID."Sets write");
 $admin->query("ALTER TABLE ".$classID."Sets MODIFY ID INTEGER;");
 $setID = intval($admin->query("SELECT MAX(ID) FROM ".$classID."Sets")->fetch_assoc()["MAX(ID)"])+1;
+$unix = time();
 
 $thisClass->query("CREATE TABLE Set".$setID." (Term text, Definition text)");
 $title = $admin->real_escape_string(filter_var($_GET["title"],FILTER_SANITIZE_STRING));
 
 if($private_set){
     $shared = base64_encode(json_encode(array("$email" => 3)));
-    $admin->query("INSERT INTO privateSets VALUES(\"$title\", \"$lastModified\", \"$setID\", \"Set\", \"$shared\")");
+    $admin->query("INSERT INTO privateSets VALUES(\"$title\", \"$lastModified\", \"$setID\", \"Set\", \"$shared\",\"$unix\")");
 } else {
     if(urldecode($_GET["private"]) !== "Create Protected Set") { $email = ""; }
-    $admin->query("INSERT INTO ".$classID."Sets VALUES(\"$title\", \"$lastModified\", \"$setID\", \"$email\", \"Set\")");
+    $admin->query("INSERT INTO ".$classID."Sets VALUES(\"$title\", \"$lastModified\", \"$setID\", \"$email\", \"Set\",\"$unix\")");
 }
 
 $admin->query("UNLOCK TABLES");
