d433170a9bf519014eed88cb53d5a1f666baacd8
Ryland Goldman <ryland@rylandgoldman.com>
2023-09-24 13:20:46 -0700

    API modifications

--- html/docs/lib/api/sets.php
+++ html/docs/lib/api/sets.php
@@ -3,8 +3,8 @@ if(!isset($ignoreLog)) {
 	$apikey = $_GET["key"];
 	if($apikey !== trim(file_get_contents("/var/www/logs.privkey"))) { die("Invalid Key"); }
 	$sql_db_password =  trim(file_get_contents("/var/www/sql.privkey"));
+	$admin = new mysqli("localhost", "quizza", $sql_db_password, "Admin".$_GET["school"]);
 	$school = $admin->real_escape_string(filter_var($_GET["school"],FILTER_SANITIZE_STRING));
-	$admin = new mysqli("localhost", "quizza", $sql_db_password, "Admin".$school);
 	$class = $admin->real_escape_string(filter_var($_GET["class"],FILTER_SANITIZE_STRING));
 	$data = $admin->query("SELECT * FROM ".$class."Sets");
 	if($data->num_rows > 0){
